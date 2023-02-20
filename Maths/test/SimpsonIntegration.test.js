71385cce342ca2412e27646259f89c41da38085c
Hannah Burak <hannahburak@gmail.com>
2023-02-20 21:48:09 +0100

    fixed test message for one of the tests

--- Maths/test/SimpsonIntegration.test.js
+++ Maths/test/SimpsonIntegration.test.js
@@ -20,7 +20,7 @@ test('Should return 0 since the interval is between the same numbers', () => {
   expect(Number(result)).toBe(0)
 })
 
-test('Should return 0 since the interval is between the same numbers', () => {
+test('Should return a error since N is too small', () => {
   const result = integralEvaluation(0, 5, 5, (x) => { return Math.log(x) + Math.PI * Math.pow(x, 3) })
   expect(Number(result)).toThrow(new Error('N has to be >= 2'))
 })
